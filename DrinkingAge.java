51d09cda35a01c20a539bdf5a27ebabfaa0148a7
Eason <liy@wlhs.wlwv.k12.or.us>
2019-10-03 09:28:14 -0700

    modified drinkingage

--- DrinkingAge.java
+++ DrinkingAge.java
@@ -1,6 +1,12 @@
 import java.util.java;
 public class DrinkingAge{
   public static void main(String[]args){
-    
+    Scanner sc=new Scanner;
+    System.out.println(System.In);
+    int age=sc.nextInt();
+    if(age>=21);
+    System.out.println("You are allowed to drink alchohol,party time!");
+    else
+    System.out.println("You are not allowed to drink alchohol yet kido:)");
   }
 }
